e5700d435fd64ec80fc05ef3d940ed169fd74f89
Rio Ikhsan <mrikhsankamal@gmail.com>
2024-06-16 21:02:29 +0700

    add playerpairingview2, function image assignment still on progress

---
+++ MiniChallenge2/MiniChallenge2/View/PlayerPairingView2.swift
@@ -0,0 +1,18 @@
+//
+//  PlayerPairingView2.swift
+//  MiniChallenge2
+//
+//  Created by Rio Ikhsan on 16/06/24.
+//
+
+import SwiftUI
+
+struct PlayerPairingView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlayerPairingView2()
+}
