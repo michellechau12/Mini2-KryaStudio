088121e5c88870b52832992475580400a4c8aa2a
Tania Cresentia <taniacresent@gmail.com>
2024-06-17 23:19:11 +0700

    } Fixes

--- MiniChallenge2/MiniChallenge2/MultipeerConnectivity/MultipeerConnectionManager.swift
+++ MiniChallenge2/MiniChallenge2/MultipeerConnectivity/MultipeerConnectionManager.swift
@@ -165,20 +165,13 @@ extension MultipeerConnectionManager: MCNearbyServiceAdvertiserDelegate {
 
 extension MultipeerConnectionManager: MCSessionDelegate {
     func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
-        <#code#>
-    }
-    
-    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
-        <#code#>
-    }
-    
-    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
         
     }
     
     func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: (any Error)?) {
         
     }
+    
     
     // this function will handle connection
     func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
@@ -218,13 +211,13 @@ extension MultipeerConnectionManager: MCSessionDelegate {
             }
             
         }
-        
-        func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) { }
-        
-        func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) { }
-        
-        func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: (Error?)?) { }
     }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) { }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) { }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: (Error?)?) { }
 }
     
     //import MultipeerConnectivity
